9ad2a3cccfdb02fd2963de70d1be48be6234bc5a
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-01-10 17:29:58 +0100

    final code

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@
 import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 import { Todo } from './types/Todo';
-
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [loading, setLoading] = useState(false);
@@ -17,18 +15,18 @@ export const App: React.FC = () => {
   const [filter, setFilter] = useState('all');
   const [modal, setModal] = useState(false);
   const [todo, setTodo] = useState<Todo | null>(null);
-
+  const resetTodo = () => {
+    setTodo(null);
+  };
   useEffect(() => {
     const getTodos = async () => {
       setLoading(true);
       setError(null);
-
       try {
         const response = await fetch('/api/todos.json');
         if (!response.ok) {
           throw new Error('Failed to fetch todos');
         }
-
         const data = await response.json();
         setTodos(data);
       } catch (err) {
@@ -37,43 +35,33 @@ export const App: React.FC = () => {
         setLoading(false);
       }
     };
-
     getTodos();
   }, []);
-
   function checkFilters(elements: Todo[]) {
     let filtered = [...elements];
-
     if (filter === 'active') {
       filtered = filtered.filter(el => el.completed === false);
     } else if (filter === 'completed') {
       filtered = filtered.filter(el => el.completed === true);
     }
-
     if (query) {
       filtered = filtered.filter(el =>
         el.title.toLowerCase().includes(query.toLowerCase()),
       );
-      
     }
-
     return filtered;
   }
-
   function handleButton(todo: Todo) {
     setTodo(todo);
     setModal(true);
   }
-
   const visibleTodos = checkFilters(todos);
-
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
-
             <div className="block">
               <TodoFilter
                 onInput={setQuery}
@@ -81,7 +69,6 @@ export const App: React.FC = () => {
                 query={query}
               />
             </div>
-
             <div className="block">
               {loading && <Loader />}
               <TodoList
@@ -93,7 +80,7 @@ export const App: React.FC = () => {
           </div>
         </div>
       </div>
-      {modal && todo && <TodoModal todo={todo} onClose={setModal} />}
+      {modal && todo && <TodoModal todo={todo} onClose={resetTodo} />}
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -2,30 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader/Loader';
 import { Todo } from '../../types/Todo';
 import { User } from '../../types/User';
-
+import { getUser } from '../../api';
 type Props = {
   todo: Todo;
-  onClose: (status: boolean) => void;
+  onClose: () => void;
 };
-
 export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
   const [modalError, setModalError] = useState(false);
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(false);
-
   const findInfo = async () => {
     setModalError(false);
     setLoading(true);
-
     try {
-      const response = await fetch('/api/users.json');
-      if (!response.ok) {
-        throw new Error('Failed to fetch users');
-      }
-
-      const data: User[] = await response.json();
-      const currentUser = data.find(user => user.id === todo.userId);
-
+      const currentUser = await getUser(todo.userId);
       if (currentUser) {
         setUser(currentUser);
       } else {
@@ -37,57 +27,56 @@ export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
       setLoading(false);
     }
   };
-
   useEffect(() => {
     findInfo();
   }, [todo]);
-
   return (
-    
-    <div className={`modal ${modalError || loading || user ? "modal is-active" : ""}`} data-cy="modal">
+    <div
+      className={`modal ${modalError || loading || user ? 'modal is-active' : ''}`}
+      data-cy="modal"
+    >
       <div className="modal-background" />
-      
-      {loading ? <Loader /> : <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            {`Todo #${todo.id}`}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${todo.id}`}
+            </div>
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={onClose}
+            />
+          </header>
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {todo.title}
+            </p>
+            <p className="block" data-cy="modal-user">
+              {todo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+              {' by '}
+              {user ? (
+                <a href={`mailto:${user.email}`}>{user.name}</a>
+              ) : modalError ? (
+                <span>User not found</span>
+              ) : (
+                <span>Loading user...</span>
+              )}
+            </p>
           </div>
-
-          <button
-            type="button"
-            className="delete"
-            data-cy="modal-close"
-            onClick={() => onClose(false)}
-          />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            {todo.title}
-          </p>
-
-          <p className="block" data-cy="modal-user">
-            {todo.completed ? (
-              <strong className="has-text-success">Done</strong>
-            ) : (
-              <strong className="has-text-danger">Planned</strong>
-            )}
-
-            {' by '}
-
-            {user ? (
-              <a href={`mailto:${user.email}`}>{user.name}</a>
-            ) : modalError ? (
-              <span>User not found</span>
-            ) : (
-              <span>Loading user...</span>
-            )}
-          </p>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
+
